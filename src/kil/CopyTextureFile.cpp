86642adbc0cd4b47cfd3b589e32aaf58a8031737
kioku <gkioku@gmail.com>
2018-07-15 15:13:31 +0900

    add copyfile function for mac and linux

--- src/kil/CopyTextureFile.cpp
+++ src/kil/CopyTextureFile.cpp
@@ -8,6 +8,70 @@
 #endif
 
 
+#ifndef _WIN32 // linux / macOS
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+int copyfile(const char *from, const char *to)
+{
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+    fd_from = open(from, O_RDONLY);
+    if (fd_from < 0)
+        return -1;
+
+    fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
+    if (fd_to < 0)
+        goto out_error;
+
+    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+    {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+
+            if (nwritten >= 0)
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if (errno != EINTR)
+            {
+                goto out_error;
+            }
+        } while (nread > 0);
+    }
+
+    if (nread == 0)
+    {
+        if (close(fd_to) < 0)
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close(fd_from);
+
+        /* Success! */
+        return 0;
+    }
+
+out_error:
+    saved_errno = errno;
+
+    close(fd_from);
+    if (fd_to >= 0)
+        close(fd_to);
+
+    errno = saved_errno;
+    return -1;
+}
+
+#endif
+
 namespace kil
 {
 	static
@@ -23,6 +87,15 @@ namespace kil
 	{
 #ifdef _WIN32
 		if (::CopyFileA(orgPath.c_str(), dstPath.c_str(), TRUE))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+#else
+		if (copyfile(orgPath.c_str(), dstPath.c_str()))
 		{
 			return true;
 		}
